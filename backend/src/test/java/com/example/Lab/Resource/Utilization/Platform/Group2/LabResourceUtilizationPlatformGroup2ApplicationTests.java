5bb989125740283de49e052796e5c807921b6d81
kongara vaibhav <kongaravaibhav8@gmail.com>
2026-07-01 22:05:34 +0530

    backend springboot

---
+++ backend/src/test/java/com/example/Lab/Resource/Utilization/Platform/Group2/LabResourceUtilizationPlatformGroup2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lab.Resource.Utilization.Platform.Group2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabResourceUtilizationPlatformGroup2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
